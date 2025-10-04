c24a6a96c719a74bc74584e9b1b7733d57d425c7
awyea <awyeah1+git@gmail.com>
2025-10-04 15:57:14 +0800

    Make bird icon increment counter and play sound; FAB changes background color to random

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'dart:math';
 
 class HomeScreen extends StatefulWidget {
   final String title;
@@ -11,16 +12,26 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _counter = 0;
+  Color _backgroundColor = Colors.purple[50]!; // initial color (matches screenshot)
   final AudioPlayer _player = AudioPlayer();
+  final Random _random = Random();
 
-  void _incrementCounter() {
+  void _onIconTap() async {
     setState(() {
       _counter++;
     });
+    await _player.play(AssetSource('audio/wagtail-chirp4.mp3'));
   }
 
-  void _playChirp() async {
-    await _player.play(AssetSource('audio/wagtail-chirp4.mp3'));
+  void _changeBackgroundColor() {
+    setState(() {
+      _backgroundColor = Color.fromARGB(
+        255,
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    });
   }
 
   @override
@@ -30,29 +41,32 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(widget.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: _playChirp,
-              child: Image.asset(
-                'assets/images/wagtail_icon.png',
-                width: 120,
-                height: 120,
+      body: Container(
+        color: _backgroundColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: _onIconTap,
+                child: Image.asset(
+                  'assets/images/wagtail_icon.png',
+                  width: 120,
+                  height: 120,
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text('Welcome to AAbird Home Screen!'),
-            const SizedBox(height: 24),
-            Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
+              const SizedBox(height: 16),
+              const Text('Welcome to AAbird Home Screen!'),
+              const SizedBox(height: 24),
+              Text('You have pushed the bird this many times:'),
+              Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _changeBackgroundColor,
+        tooltip: 'Change background',
         child: const Icon(Icons.add),
       ),
     );
